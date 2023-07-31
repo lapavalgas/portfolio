be830e5d22d0fa37cb1adbaebe2b0c0506c48911
rafael <lapavalgas@gmail.com>
2023-07-31 14:46:46 -0300

    json patterns clean

--- code/app/portfolio/src/assets/import_external.ts
+++ code/app/portfolio/src/assets/import_external.ts
@@ -3,3 +3,4 @@
 //     "https://raw.githubusercontent.com/lapavalgas/portfolio/feature/layout-v0.1.0/code/app/portfolio/src/data/languages.json?token=GHSAT0AAAAAACETSK7RTIXYIVBKYL5F3M3KZGHURLA"
 //   )
 // ).json();
+export {};
